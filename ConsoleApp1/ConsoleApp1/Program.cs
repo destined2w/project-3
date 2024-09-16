4516166918d48f84d5ae0016c3e4f4013fda180c
destined2w <notaverageprod@gmail.com>
2024-09-16 17:18:41 +0500

    3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое слагаемое");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе слагаемое");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Сумма чисел равна " + (x + y));
+            Console.ReadKey();
+        }
+    }
+}
