7bd50763cac599a47d7cb47722b44b7585de88b8
destined2w <notaverageprod@gmail.com>
2024-09-30 16:53:56 +0500

    программа для вычисления объема и длины диагонали куба

---
+++ practika3009/practika3009/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practika3009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длину ребра куба:");
+            double rebro = double.Parse(Console.ReadLine());
+            double size = Math.Pow(rebro, 3);
+            double length = rebro * Math.Sqrt(3);
+            Console.WriteLine("Объем куба: " + size);
+            Console.WriteLine("Длина диагонали куба: " + length);
+
+
+            Console.ReadKey();
+        }
+    }
+}
