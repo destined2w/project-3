798206cd1b9d7a9c68d92c5f4e7a64aa0f7eae78
Егор Бобин <notaverageprod@gmail.com>
2024-10-05 17:46:15 +0500

    программа вычисляющая через сколько минут совпадут стрелки часов

---
+++ task003/task003/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task003
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] time = { 0, 65, 130, 195, 260, 325, 390, 455, 520, 585, 650, 715 };
+            Console.WriteLine("Введите часы: ");
+            int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите минуты: ");
+            int n = int.Parse(Console.ReadLine());
+            int t = m * 60 + n;
+            foreach (int i in time)
+            {
+                if (i > t) {
+                    Console.WriteLine("Стрелки совпадут через: " + (i - t) + " минут");
+                    break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
