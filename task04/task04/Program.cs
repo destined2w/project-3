e31b7f5c18cf0ff7c8a78510c4dcc2aee02ce7e7
Егор Бобин <notaverageprod@gmail.com>
2024-10-14 18:00:42 +0500

    сделал программу для вычисления х

---
+++ task04/task04/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите x:");
+
+            double x = double.Parse(Console.ReadLine());
+            double y = Math.Sqrt((2 * x + Math.Sin(Math.Abs(3 * x))) / 3.56);
+
+            Console.WriteLine("Результат: " + y);
+            Console.ReadKey();
+        }
+    }
+}
