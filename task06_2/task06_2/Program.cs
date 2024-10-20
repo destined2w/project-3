1a2d64212dac2d6bc7c81b78dd68d5da13476bc5
Егор Бобин <notaverageprod@gmail.com>
2024-10-20 18:11:09 +0500

    сделал программу для преобразования слова трос (вариант 3)

---
+++ task06_2/task06_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task06_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var word = "трос";
+            var word1 = new string(word.Reverse().ToArray());
+            var word2 = word.Remove(0, 1) + word.Remove(1, 3); 
+            Console.WriteLine("Исходное слово: " + word);
+            Console.WriteLine("Первое преобразованное слово: " + word1);
+            Console.WriteLine("Второе преобразованное слово: " + word2);
+            Console.ReadKey();
+        }
+    }
+}
