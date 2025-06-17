34f9c502438d2902e87474328f1d640309889b64
Егор Бобин <notaverageprod@gmail.com>
2025-06-17 21:51:35 +0500

    выполнил задание экзамена

---
+++ HuntSolitaire/HuntSolitaire/Card.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HuntSolitaire
+{
+    public class Card
+    {
+        public string Rank { get; }
+        public string Suit { get; }
+        public Image Image { get; }
+
+        public Card(string rank, string suit, Image image)
+        {
+            Rank = rank;
+            Suit = suit;
+            Image = image;
+        }
+
+        public static Image LoadCardImage(string rank, string suit)
+        {
+            string imageName = $"{rank}_of_{suit}";
+            try
+            {
+                return Image.FromFile($"Cards/{imageName}.png");
+            }
+            catch
+            {
+                // Если изображение не найдено, создаем placeholder
+                return CreatePlaceholderImage(rank, suit);
+            }
+        }
+
+        private static Image CreatePlaceholderImage(string rank, string suit)
+        {
+            Bitmap bmp = new Bitmap(71, 96);
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+                using (Font font = new Font("Arial", 10))
+                {
+                    g.DrawString($"{rank} of {suit}", font, Brushes.Black, new PointF(5, 40));
+                }
+            }
+            return bmp;
+        }
+    }
+}
---
+++ HuntSolitaire/HuntSolitaire/RulesForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuntSolitaire
+{
+    public partial class RulesForm : Form
+    {
+        public RulesForm()
+        {
+            InitializeComponent();
+            Text = "Правила пасьянса 'Охота'";
+            Size = new Size(400, 300);
+
+            TextBox textBox = new TextBox
+            {
+                Multiline = true,
+                Dock = DockStyle.Fill,
+                ReadOnly = true,
+                Text = "Правила пасьянса 'Охота':\n\n" +
+                       "1. Используется колода из 36 карт\n" +
+                       "2. Карты раскладываются в 9 стопок по 4 карты\n" +
+                       "3. Игрок может убирать пары карт одинакового достоинства\n" +
+                       "4. Цель игры - убрать все карты с поля"
+            };
+
+            Controls.Add(textBox);
+        }
+    }
+}
